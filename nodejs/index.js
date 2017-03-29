002e6b09bd9e80fbac38988bf9d3a9b744b838b0
mv7856 <mv7856@student.uni-lj.si>
2017-03-30 00:36:46 +0200

    simple http server

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()  
 const port = process.env.PORT || 8080;
 
+
 app.get('/', (request, response) => {  
   response.send('Hello from Express!')
 })
